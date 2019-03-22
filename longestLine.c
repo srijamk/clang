0918f4e9707d6f49c9e9f7830e5ac96de303d6da
Srija Makkapati <srijamk@gmail.com>
2019-03-22 07:05:36 -0400

    added comments

--- longestLine.c
+++ longestLine.c
@@ -11,14 +11,14 @@ main() {
     char longest[MAXLINE];
 
     max = 0;
-    while ((len = get_line(line, MAXLINE)) > 0) {
-        if (len > max) {
-            max = len;
-            copy(longest, line);
+    while ((len = get_line(line, MAXLINE)) > 0) {   // while we have another line
+        if (len > max) { 
+            max = len;  // update max
+            copy(line, longest);    // record the longest line
         }
     }
     if (max > 0) {
-        printf("%s", longest);
+        printf("%s", longest);  // just print the longest line
     }
     return 0;
 }
@@ -26,22 +26,22 @@ main() {
 int get_line (char s[], int lim) {
     int c, i;
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {  // records up to MAXLINE chars of next line
         s[i] = c;
     }
-    if (c == '\n') {
+    if (c == '\n') {    // checks for end of line
         s[i] = c;
-        ++i;
+        ++i;    // moves to next char to prepare for end of line
     }
-    s[i] = '\0';
+    s[i] = '\0';    // adds end of line signal
     return i;
 }
 
-void copy (char to[], char from[]) {
+void copy (char from[], char to[]) {
     int i;
 
     i = 0;
-    while ((to[i] = from[i]) != '\0') {
+    while ((to[i] = from[i]) != '\0') {     // assigns from[] values to to[] values and simultaneously checks for end of line
         ++i;
     }
 }
