87e923824404f96eb3cecd3e5938c3ddb476dacf
Kiran Makkapati <srijamk@gmail.com>
2019-06-10 17:51:44 -0400

    structures

--- course_project.c
+++ course_project.c
@@ -2,15 +2,30 @@
 #define MAX_TITLE_LENGTH 100
 #define MAX_ARTIST_LENGTH 50
 #define MAX_CDS 3
+#define NO_CDS 100
 
-char title[MAX_TITLE_LENGTH], artist[MAX_ARTIST_LENGTH];
-float price;
-int num_tracks, is_album;
-
-void output(char title[], char artist[], int num_tracks, char album_or_single, float price)
+struct cd_type
 {
-  printf("\nCD Details:\n============================\nTitle: %s\nArtist: %s\nNumber of Tracks: %d\n", title, artist, num_tracks);
-  (album_or_single == 'a' || album_or_single == 'A') ? printf("Type: Album\nPrice: %.2f\n============================\n", price) : printf("Type: Single\nPrice: %.2f\n============================\n", price);
+  char title[MAX_TITLE_LENGTH];
+#ifndef NOARTIST
+  char artist[MAX_ARTIST_LENGTH];
+#endif
+  int num_tracks;
+  char album_or_single;
+  float price;
+};
+
+typedef struct cd_type cd_t;
+
+void enter(char prompt[]);
+int read_int(char prompt[]);
+cd_t read_cd();
+void print_cd(cd_t cd);
+
+void print_cd(cd_t cd)
+{
+  printf("\nCD Details:\n============================\nTitle: %s\nArtist: %s\nNumber of Tracks: %d\n", cd.title, cd.artist, cd.num_tracks);
+  (cd.album_or_single == 'a' || cd.album_or_single == 'A') ? printf("Type: Album\nPrice: %.2f\n============================\n", cd.price) : printf("Type: Single\nPrice: %.2f\n============================\n", cd.price);
   getchar();
 }
 
@@ -28,44 +43,57 @@ void say_goodbye()
   printf("Goodbye!\n");
 }
 
-void input()
+cd_t read_cd()
 {
+  cd_t cd;
   printf("Please enter your CD details below.\n");
 
   printf("Type in the title of your CD: ");
-  scanf("%[^\n]", title);
+  scanf("%[^\n]", cd.title);
   getchar();
 
   printf("Type in the artist of your CD: ");
-  scanf("%[^\n]", artist);
+  scanf("%[^\n]", cd.artist);
   getchar();
 
   printf("Type in the number of tracks on your CD: ");
-  scanf("%d", &num_tracks);
+  scanf("%d", &cd.num_tracks);
   getchar();
 
   printf("Type in the price of your CD: ");
-  scanf("%f", &price);
+  scanf("%f", &cd.price);
   getchar();
+
+  return cd;
 }
 
 int main()
 {
+
   char album_or_single, another_cd = 'x';
-  int num_cds = 1;
+  cd_t cds[NO_CDS];
+  int num_cds = 0;
 
   printf("Welcome to your CD Database.\n\n");
 
   while (another_cd != 'n' && another_cd != 'N')
   {
-    input();
+    read_cd();
     album_or_single = yesno("If your CD is an album, type 'A'. Otherwise, type 'S'.", 'A', 'S');
-    output(title, artist, num_tracks, album_or_single, price);
+    cds[num_cds] = read_cd();
+    getchar();
+
     another_cd = yesno("Would you like to provide the details for another CD? (y/n) ", 'y', 'n');
     getchar();
 
     if (another_cd == 'y')
       (num_cds < MAX_CDS) ? ++num_cds : printf("You have already entered details for %d CDs.\n", MAX_CDS);
+  }
+
+  for (int i = 0; i < num_cds; i++)
+  {
+    printf("The details of CD #%d\n are: \n", i + 1);
+    print_cd(cds[i]);
   }
 
   say_goodbye();
