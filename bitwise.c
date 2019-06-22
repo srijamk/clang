31e8ce2aa8886cae42b354c9ca5fa6388f7a33e6
Srija Makkapati <srija@Kirans-MacBook-Pro.local>
2019-06-22 09:05:39 -0400

    experimenting with turning bits off/on

--- bitwise.c
+++ bitwise.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+#define BIT     (1 << 2)
+
 int main() {
-    int x = 6;
-    printf("%d shifted 3 left = %d", x, x << 3);
+   int x = 5;
+    
+    printf("%d\n", x);
+    // 00000101 | 00001000 = 00001101
+    // 00000101 & ~00000100 = 00000101 & 00000001 = 00000001 
+    printf("%d\n", BIT);
+    printf("%d\n", x & ~BIT);
 }
