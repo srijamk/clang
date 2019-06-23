0f962c4f6036cf2089bb11dcab1e40e70b7ccecc
Srija <srijamk@gmail.com>
2019-06-23 12:05:26 -0400

    htoi

--- htoi.c
+++ htoi.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int htoi(char s[]) {
+    int result, cur = 0;
+
+    for (int i = strlen(s) - 1; i >= 0; i--) {
+        (isdigit(s[i])) && (result += pow(16, cur) * (s[i] - '0'));
+        if (!isdigit(s[i])) {
+            if (s[i] >= 'a' && s[i] <= 'z')
+                result += pow(16, cur) * (s[i] - 'a' + 10);
+            else
+                result += pow(16, cur) * (s[i] - 'A' + 10);
+        }
+        cur++;
+    }
+    return result;
+}
 
 int main() {
-    printf("%d\n", (int) '0');
-    printf("%d\n", (int) 'a');
-    printf("%c\n", (char) 1);
+   // 0 - 9 converts to 0 - 9
+   // a - f and A - F converts to 10 - 15
+    char line[100];
+    scanf("%s", line);
+    printf("%d\n", htoi(line));
 }
