a436dceb6b195d9eac2de55eef8494dcb99f417b
Srija <srijamk@gmail.com>
2019-06-24 20:37:10 -0400

    condensed expand program

--- expand.c
+++ expand.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
+#define MAX_LENGTH      100
 
 int main() {
-    char s1[100], s2[100];
-    int i, j = 0;
+    char s1[MAX_LENGTH], s2[MAX_LENGTH];
+    int i, j, k = 0;
     
     scanf("%s", s1);
     while (s1[i] != '\0') {
         if (s1[i] == '-') {
             if (i > 0 && s1[i - 1] != '-' && s1[i + 1] != '-') {
-                for (int k = s1[i - 1]; k <= s1[i + 1]; k++) {
-                    s2[j++] = k; 
+                k = ((s1[i - 2] != '-') ? s1[i - 1] : s1[i - 1] + 1);
+                while (k <= s1[i + 1]) {
+                    s2[j++] = k++; 
                 }
-                i += 2;
-            }
-            else
                 i++;
+            }
         }
         else if (s1[i + 1] != '-') {
             s2[j++] = s1[i];
-            i++;
         }
-        else
-            i++;
+        i++;
     }   
 
     printf("%s\n", s2);
